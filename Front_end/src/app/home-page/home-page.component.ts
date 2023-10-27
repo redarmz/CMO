c63d57480b706bf62dd96e2dd3704240124fcf65
reda <reda.ramzi0104@hotmail.com>
2023-10-27 20:00:21 +0200

    init

---
+++ Front_end/src/app/home-page/home-page.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
+})
+export class HomePageComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  imageUrl!: string;
+
+  //Initialisation des attributs
+  ngOnInit(): void {
+    this.title = 'ça mange où';
+    this.description = 'Voici notre application web révolutionnaire qui vous sera utile pour toutes vos pauses déj !';
+    this.createdDate = new Date();
+    this.imageUrl = 'https://static.750g.com/images/600-600/f3cb23d0e2e9319c4e698badffb18c89/10-plats-quon-mange-avec-les-doigts.jpg';
+  }
+}
