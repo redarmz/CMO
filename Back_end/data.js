8d1d984f4982e695ae2b10d3489238c72fd875f9
reda <reda.ramzi0104@hotmail.com>
2023-10-29 15:14:45 +0100

    ajout du fichier data.js fonctionel

--- Back_end/data.js
+++ Back_end/data.js
@@ -3,7 +3,7 @@
 /*const fakeRestaurants = [
     { id: 1, name: 'Restaurant A' },
     { id: 2, name: 'Restaurant B' },
-    // Ajoutez d'autres restaurants fictifs ici
+    //Ajoutez d'autres restaurants fictifs ici
   ];
   
   module.exports = {
@@ -13,8 +13,9 @@
 const restaurantsData = [
   { id: 1, name: "Fast Food :  Pitaya" },
   { id: 2, name: "Fast Food :  Point B" },
-  { id: 3, name: "Restaurant : Le Fouquet's" },
-  { id: 4, name: "Restaurant : Ciel de Paris" },
+  { id: 3, name: "Fast Food : BurgerKing" },
+  { id: 4, name: "Restaurant : Le Fouquet's" },
+  { id: 5, name: "Restaurant : Ciel de Paris" },
   // Ajoutez d'autres restaurants ici
 ];
 
