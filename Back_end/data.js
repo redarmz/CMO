bbb62af1e6ad3a4b2c7d5e3df7d2621fff150c1b
WassimH94 <wassim.hendali2@gmail.com>
2023-11-25 19:08:37 +0100

    Ajout des données du forms dans le back

--- Back_end/data.js
+++ Back_end/data.js
@@ -1,14 +1,50 @@
-//Fichier servant de base de données en attendant l'ajout d'une BD
-
-const restaurantsData = [
-  { id: 1, name: "Fast Food :  Pitaya" },
-  { id: 2, name: "Fast Food :  Point B" },
-  { id: 3, name: "Fast Food : BurgerKing" },
-  { id: 4, name: "Restaurant : Le Fouquet's" },
-  { id: 5, name: "Restaurant : Ciel de Paris" },
-  // Ajoutez d'autres restaurants ici
-];
-
 module.exports = {
-  restaurants: restaurantsData,
+  "restaurants": [
+    {
+      "id": 1,
+      "name": "Fast Food :  Pitaya"
+    },
+    {
+      "id": 2,
+      "name": "Fast Food :  Point B"
+    },
+    {
+      "id": 3,
+      "name": "Fast Food : BurgerKing"
+    },
+    {
+      "id": 4,
+      "name": "Restaurant : Le Fouquet's"
+    },
+    {
+      "id": 5,
+      "name": "Restaurant : Ciel de Paris"
+    }
+  ],
+  "events": [
+    {
+      "id": 1,
+      "userName": "Wassim",
+      "eventName": "test",
+      "guestList": [],
+      "description": "event 123467",
+      "numeroEvent": 123467
+    },
+    {
+      "id": 2,
+      "userName": "final",
+      "eventName": "final",
+      "guestList": [],
+      "description": "final",
+      "numeroEvent": 3333
+    },
+    {
+      "id": 3,
+      "userName": "test",
+      "eventName": "test",
+      "guestList": "Reda",
+      "description": "test",
+      "numeroEvent": 1111
+    }
+  ]
 };
--- Back_end/server.js
+++ Back_end/server.js
@@ -1,17 +1,39 @@
-
-//Version avec fichier data.js implémenter
+const fs = require('fs');
 const http = require('http');
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
-const restaurantsData = require('./data'); // Importez les données des restaurants
+const eventData = require('./data'); // Importez les données d'événement depuis data.js
 
 app.use(cors({ origin: 'http://localhost:4200' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/restaurants', (req, res) => {
-  // Utilisez les données des restaurants importées depuis data.js
-  res.json(restaurantsData.restaurants);
+  res.json(eventData.restaurants);
+});
+
+app.post('/create-event', (req, res) => {
+  const newEventData = req.body;
+
+  // Générez un nouvel ID pour le nouvel événement
+  const newEventId = eventData.events.length + 1;
+
+  // Ajoutez le nouvel événement à la liste des événements dans eventData
+  eventData.events.push({ id: newEventId, ...newEventData });
+
+  // Mettez à jour le fichier data.js avec les nouvelles données
+  const updatedDataFile = `module.exports = ${JSON.stringify(eventData, null, 2)};`;
+
+  fs.writeFile('data.js', updatedDataFile, (err) => {
+    if (err) {
+      console.error('Erreur lors de l\'écriture du fichier:', err);
+      res.status(500).json({ message: 'Erreur lors de la mise à jour des données.' });
+    } else {
+      console.log('Données mises à jour avec succès dans data.js');
+      res.status(200).json({ message: 'Événement créé avec succès!', event: { id: newEventId, ...newEventData } });
+    }
+  });
 });
 
 const port = process.env.PORT || 3000;
@@ -19,6 +41,5 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 server.listen(port, () => {
-  
   console.log(`Serveur Node.js écoutant sur le port ${port}`);
 });
