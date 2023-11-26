45bff09c1c3547bac3a624e70fc1808604a7902a
reda <reda.ramzi0104@hotmail.com>
2023-11-26 17:45:50 +0100

    donnée decharger

--- Back_end/data.js
+++ Back_end/data.js
@@ -24,94 +24,13 @@ module.exports = {
   "events": [
     {
       "id": 1,
-      "userName": "Wassim",
-      "eventName": "test",
-      "guestList": [
-        "wassim"
-      ],
-      "description": "event 123467",
-      "numeroEvent": 123467
-    },
-    {
-      "id": 2,
-      "userName": "final",
-      "eventName": "final",
-      "guestList": [],
-      "description": "final",
-      "numeroEvent": 3333
-    },
-    {
-      "id": 3,
-      "userName": "Benz",
-      "eventName": "test",
-      "guestList": "Modric",
-      "description": "e",
-      "numeroEvent": 1209
-    },
-    {
-      "id": 4,
-      "userName": "Benzema",
-      "eventName": "LDC",
-      "guestList": "Modric",
-      "description": "14 salll nueve",
-      "numeroEvent": 6577
-    },
-    {
-      "id": 5,
-      "userName": "Paul",
-      "eventName": "Anniversaire ",
-      "guestList": [
-        "laure",
-        "phil",
-        "amir"
-      ],
-      "description": "Un anniv",
-      "numeroEvent": 1310
-    },
-    {
-      "id": 6,
-      "userName": "GHG",
-      "eventName": "SSS",
-      "guestList": "S",
-      "description": "CD",
-      "numeroEvent": 1234
-    },
-    {
-      "id": 7,
-      "userName": "d",
-      "eventName": "d",
-      "guestList": "d",
-      "description": "d",
-      "numeroEvent": 1234
-    },
-    {
-      "id": 8,
-      "userName": "a",
-      "eventName": "a",
-      "guestList": "a",
-      "description": "a",
-      "numeroEvent": 5678
-    },
-    {
-      "id": 9,
-      "userName": "Toto",
-      "eventName": "fete des totos",
+      "userName": "Patrick",
+      "eventName": "Saint Patrick",
       "guestList": [
         "Reda"
       ],
-      "description": "tooootttttooo",
-      "numeroEvent": 8899
-    },
-    {
-      "id": 10,
-      "userName": "Lina",
-      "eventName": "Fete des mere",
-      "guestList": [
-        "Chris",
-        "Paul"
-      ],
-      "description": "vive les mamans",
-      "numeroEvent": 6603
+      "description": "Avec modération",
+      "numeroEvent": 9093
     }
   ]
 };
