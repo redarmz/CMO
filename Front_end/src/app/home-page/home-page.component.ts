f7563898c49893756200bd753f614449211fb9ec
reda <reda.ramzi0104@hotmail.com>
2023-12-20 10:36:32 +0100

    amélioration home

--- Front_end/src/app/home-page/home-page.component.ts
+++ Front_end/src/app/home-page/home-page.component.ts
@@ -24,6 +24,7 @@ export class HomePageComponent implements OnInit {
     this.description = 'Voici notre application web révolutionnaire qui vous sera utile pour toutes vos pauses déj !';
     this.createdDate = new Date();
     this.imageUrl = 'https://static.750g.com/images/600-600/f3cb23d0e2e9319c4e698badffb18c89/10-plats-quon-mange-avec-les-doigts.jpg';
+    this.animateTitle();
 
     // Récupération des données fictives depuis le service
     this.restaurantService.getRestaurants().subscribe((data: any) => {
@@ -44,5 +45,15 @@ export class HomePageComponent implements OnInit {
   // Fonction pour rediriger vers la page de reservation
   redirectToReservation() {
     this.router.navigate(['/reserve-resto']);
+  }
+
+  animateTitle() {
+    const titleElement = document.querySelector('.header h1') as HTMLElement;
+    if (titleElement) {
+      titleElement.style.opacity = '0'; // Commencer avec une opacité de 0
+      setTimeout(() => {
+        titleElement.style.opacity = '1'; // Augmenter l'opacité après un délai
+      }, 500);
+    }
   }
 }
