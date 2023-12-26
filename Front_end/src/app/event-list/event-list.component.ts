40231f8e75afc1520be1208a451a544d90e6b607
reda <reda.ramzi0104@hotmail.com>
2023-12-26 16:02:33 +0100

    amélioration recherche d'event

--- Front_end/src/app/event-list/event-list.component.ts
+++ Front_end/src/app/event-list/event-list.component.ts
@@ -1,35 +1,3 @@
-/*import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'; // Importez le service Router
-import { EventService } from '../event.service';
-
-
-@Component({
-  selector: 'app-event-list',
-  templateUrl: './event-list.component.html',
-  styleUrls: ['./event-list.component.scss']
-})
-export class EventListComponent implements OnInit{
-
-  events: any[] = []; // Propriété pour stocker les données fictives
-
-  constructor(private router: Router, private eventService: EventService) {
-    // Injection du service Router et EventService 
-  }
-
-  ngOnInit() {
-    this.eventService.getEvents().subscribe((data: any) => {
-      this.events = data;
-    });
-  }
-
-  // Fonction pour rediriger vers la page "d'acceuil"
-  redirectToCodeInvit() {
-    this.router.navigate(['/event-invit']);
-  }
-
-}*/
-
-
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormControl } from '@angular/forms';
