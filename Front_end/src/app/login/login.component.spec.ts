8bc50b2e800b7b9b496ea6fd135ac0ffc4cca835
reda <reda.ramzi0104@hotmail.com>
2024-02-16 17:30:14 +0100

    V1.0

--- Front_end/src/app/login/login.component.spec.ts
+++ Front_end/src/app/login/login.component.spec.ts
@@ -1,28 +1,3 @@
-/*import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './login.component';
-
-describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [LoginComponent],
-      imports: [HttpClientModule,ReactiveFormsModule]
-    });
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
-*/
-
 import { ComponentFixture, TestBed, waitForAsync, fakeAsync, tick } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
